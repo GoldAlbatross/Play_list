739405bbebb83e2bd5a95f92ae1a5135fb39fb3c
GoldAlbatross <snovaodin@gmail.com>
2022-12-24 20:02:55 +0300

    добавил переход при нажатии на кнопки

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.practicum.playlistmaker
-/*
-git init
-git add .
-git commit -m "message"
-git remote add origin link
-git push -u origin master
-git checkout -b dev
- */
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,20 +13,20 @@ class MainActivity : AppCompatActivity() {
 
         val btnSearch = findViewById<Button>(R.id.btn_search)
         val btnClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) { Toast
-                .makeText(this@MainActivity, "Поиск!", Toast.LENGTH_LONG)
-                .show() }
+            override fun onClick(v: View?) {
+                startActivity(Intent(this@MainActivity, Search::class.java))
+            }
         }
         btnSearch.setOnClickListener(btnClickListener)
 
         val btnMediaLib = findViewById<Button>(R.id.btn_media)
-        btnMediaLib.setOnClickListener { Toast
-            .makeText(this@MainActivity, "Медиатека!", Toast.LENGTH_LONG)
-            .show() }
+        btnMediaLib.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MediaLib::class.java))
+        }
 
         val btnSettings = findViewById<Button>(R.id.btn_settings)
-        btnSettings.setOnClickListener { Toast
-            .makeText(this@MainActivity, "Настройки!", Toast.LENGTH_LONG)
-            .show() }
+        btnSettings.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+        }
     }
 }
