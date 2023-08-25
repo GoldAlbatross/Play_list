003bd07b940dada9625887766ba41ed5ae112cb2
Petr Gnesny <snovaodin@gmail.com>
2023-08-25 14:12:05 +0300

    done: libs.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ksp { arg("room.schemaLocation", "$projectDir/schemas") }
     }
 
     buildFeatures {
@@ -35,12 +37,12 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
         debug {
-            buildConfigField("String","test", "\"testInformation\"")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
-    java { toolchain.languageVersion.set(JavaLanguageVersion.of(17)) }
-    kotlinOptions { jvmTarget = "17" }
+    java { toolchain.languageVersion.set(JavaLanguageVersion.of(18)) }
+    kotlinOptions { jvmTarget = "18" }
 
 }
 
@@ -54,37 +56,18 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // Kotlinx.Serialization
+    implementation(libs.bundles.navigation.component)
     implementation(libs.bundles.serialization)
-
-    //retrofit + converter JSON/JAVA
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    //Glide: show images from network
-    ksp("com.github.bumptech.glide:compiler:4.14.2")
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-
-    //requests logging
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-
-    // Koin
-    implementation("io.insert-koin:koin-android:3.3.0")
-
-    // Jetpack Navigation Component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.0")
+    implementation(libs.coroutines.android)
+    implementation(libs.bundles.retrofit2)
+    implementation(libs.bundles.room)
+    //implementation(libs.dagger)
+    implementation(libs.glide)
+    implementation(libs.koin)
+    //ksp(libs.dagger.compiler)
+    ksp(libs.glide.compiler)
+    ksp(libs.room.compiler)
 
     // LeakCanary
     //debugImplementation 'com.squareup.leakcanary:leakcanary-android:2.8.1'
-
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
-
-    // Room
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
-    ksp("androidx.room:room-compiler:2.5.2")
-
 }
